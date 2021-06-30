9ea1aac3b95f989b905f10ed6be57889be266bbf
David Onaolapo <talk2dawich@gmail.com>
2021-06-30 21:46:53 +0000

    Modified play.js and client.js

--- client.js
+++ client.js
@@ -5,12 +5,15 @@ const connect = function () {
   const conn = net.createConnection({
     host: '10.0.2.15',
     port: 50541
-  }, () => {
-    console.log("Sucessful connection");
   });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+
+  conn.on('connect', () => {
+    console.log("Succesful Connection");
+    conn.write("Name: D.O")
+  })
 
   conn.on('data', (data) => {
    console.log(data);
--- play.js
+++ play.js
@@ -1,4 +1,36 @@
 const connect = require('./client.js')
 
 console.log("Connecting ...");
-connect();
+const play = connect();
+
+// const moves = ["Move: up", "Move: up", "Move: left", "Move: left"];
+
+// let time = 50;
+
+// for (let move of moves ) {
+//   setTimeout(() => {
+//     play.write(move);
+//   }, time)
+//   time += 50;
+// }
+
+const handleUserInput = function (data) {
+  if (data === '\u0003') {
+    play.destroy();
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on('data', handleUserInput);
+
+  return stdin;
+};
+
+setupInput();
+
